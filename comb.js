cab7919062c68ff34f03d5ad0f47c6f0b5eaa45f
ConorOBrien <csobrien@email.wm.edu>
2021-05-22 19:55:54 -0400

    add rudimentary filtering

---
+++ comb.js
@@ -0,0 +1,33 @@
+db = (function () {
+let i = 0, id = 0;
+let db = {};
+for(let table of document.querySelectorAll("table")) {
+  for(let row of table.querySelectorAll("tbody tr")) {
+    db[id] = {
+      id: id,
+      name: row.children[0].textContent.trim(),
+      rarity: row.children[2].textContent.trim(),
+      energy: null,
+      type: null,
+      description: row.children[3].textContent.trim(),
+      img: row.children[1].querySelector("a").href,
+      color: i,
+      mod: "base",
+    };
+    if(row.children[4]) {
+      db[id].type = row.children[3].textContent.trim();
+      db[id].energy = row.children[4].textContent.trim();
+      db[id].description = row.children[5].textContent.trim();
+    }
+    else {
+      db[id].type = ["Status", "Curse"][i];
+    }
+    id++;
+  }
+  i++;
+  // id = Math.max(id, 1000*i);
+  //id for internal use only
+}
+return db;
+})();
+copy(JSON.stringify(db))
---
+++ db.js
@@ -0,0 +1,68 @@
+const filterDatabase = (db, { name }) => {
+    let res = [];
+    for(let [ id, card ] of Object.entries(db)) {
+        if(card.name.indexOf(name) !== -1) {
+            res.push(card);
+        }
+    }
+    return res;
+};
+
+const clearElement = (el) => {
+    while(el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
+
+const makeElement = (tag, opts = {}, children = []) => {
+    let el = document.createElement(tag);
+    if(opts.classes) {
+        el.classList.add(...opts.classes);
+    }
+    if(opts.id) {
+        el.id = id;
+    }
+    for(let child of children) {
+        el.appendChild(child);
+    }
+    return el;
+};
+
+// const BASE_CARD_RESULT = document.createElement("div");
+const BASE_CARD_RESULT = makeElement("div", {}, [
+    makeElement("div", { classes: [ "name" ] }),
+    makeElement("div", { classes: [ "energy" ] }),
+    makeElement("div", { classes: [ "description" ] }),
+    makeElement("img", { classes: [ "thumb" ] }),
+]);
+
+const formatResult = (entity) => {
+    let c = BASE_CARD_RESULT.cloneNode(true);
+    c.querySelector(".name").textContent = entity.name;
+    c.querySelector(".energy").textContent = entity.energy;
+    c.querySelector(".description").textContent = entity.description;
+    let imthumb = c.querySelector("img.thumb");
+    imthumb.src = entity.img;
+    imthumb.width = 100;
+    return c;
+};
+
+window.addEventListener("load", async function () {
+    const baseInquiry = await fetch("./base.json");
+    const baseDb = await baseInquiry.json();
+    // console.log(baseDb[0]);
+    window.baseDb = baseDb;
+    
+    const name = document.getElementById("name");
+    const submit = document.getElementById("submit");
+    const output = document.getElementById("output");
+    
+    submit.addEventListener("click", function () {
+        let results = filterDatabase(baseDb, { name: name.value });
+        // result = result.map(formatResult);
+        clearElement(output);
+        for(let h of results.map(formatResult)) {
+            output.appendChild(h);
+        }
+    });
+});
